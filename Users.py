bf732da08866c87492962febef34221824fb2c0e
Artur Reznikov <areznikov@live.com>
2012-02-17 20:23:30 -0800

    First commit

---
+++ Users.py
@@ -0,0 +1,45 @@
+"""Server Density plugin for monitoring linux users currently on the server 
+
+Copyright 2011  Artur Reznikov areznikov@live.com. All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are
+permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of
+conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list
+of conditions and the following disclaimer in the documentation and/or other materials
+provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY ARTUR REZNIKOV ''AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL ARTUR REZNIKOV OR
+CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those of the
+authors and should not be interpreted as representing official policies, either expressed
+or implied, of Artur Reznikov.
+"""
+
+__author__ = "Artur Reznikov <areznikov@live.com>"
+__version__ = "1.0.2"
+
+import subprocess
+
+class Users:
+    def __init__(self, config, logger, raw_config):
+        self.config = config
+        self.logger = logger
+        self.raw_config = raw_config
+
+    def run(self):
+        user_count= int(subprocess.Popen(['w' '| wc -l '],stdout=subprocess.PIPE, shell=True).communicate()[0].strip())-2
+        #Counting users, we don't count first 2 lines of w output
+        return_data = {'Users': user_count }
+        return return_data
